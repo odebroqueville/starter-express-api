7809ead76dd87ce0da8cc96a51f2500d040db4ad
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2022-10-27 16:19:45 +0200

    debug test

--- index.js
+++ index.js
@@ -13,32 +13,7 @@ app.get('/:url', (req, res) => {
 app.listen(process.env.PORT || 3000);
 
 // Helper function to get the title of a web page
-async function getTitle(url){
-    try {
-        const options = {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'text/html'
-            }
-        };
-        const response = await fetch(url, options);
-        const html = await response.text();
-        let title = '';
-        const titleMatches = html.match(/<title.*?>.*?<\/title>/gmi)||[];
-        if (titleMatches.length > 0) {
-            title = titleMatches[0];
-            console.log(title);
-        }
-        if (title.search(/<title/gi) !== -1){
-            const titleText = title.substring(title.indexOf('>')+1);
-            const res = titleText.replace('</title>','');
-            console.log(res);
-            return res;
-        }
-        return '';
-    } catch (err) {
-        console.error(`Failed to retrieve title with error: ${err}`);
-        return '';
-    }
+function getTitle(url){
+    return 'Hellow World!"
+
 }
